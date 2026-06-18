c97ad6e62fdb5ad9fa0512bfabcc11e326409c4b
tomcalientodev <tc298261@gmail.com>
2026-06-18 15:37:14 -0600

    removed name from user model

--- crms_backend/accounts/models.py
+++ crms_backend/accounts/models.py
@@ -24,23 +24,23 @@ class Organization(models.Model):
 
 class CustomUserManager(UserManager):
     '''Added Further Email Verification here and Email as username in Abstract User .'''
-    def _create_user(self, name, email, password, **extra_fields):
+    def _create_user(self, email, password, **extra_fields):
         if not email: 
             raise ValueError("You have not provided a valid e-mail address")
 
         email = self.normalize_email(email)
-        user = self.model(name=name, email=email, **extra_fields)
+        user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
         return user
     
-    def create_user(self, name=None, email=None, password=None, **extra_fields):
+    def create_user(self, email=None, password=None, **extra_fields):
         extra_fields.setdefault("is_staff", False)
         extra_fields.setdefault("is_superuser", False)
-        return self._create_user(name, email, password, **extra_fields)
+        return self._create_user(email, password, **extra_fields)
     
-    def create_superuser(self, name=None, email=None, password=None, **extra_fields):
+    def create_superuser(self, email=None, password=None, **extra_fields):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
 
@@ -49,7 +49,7 @@ class CustomUserManager(UserManager):
         if extra_fields.get("is_superuser") is not True:
             raise ValueError("Superuser must have is_superuser=True.")
 
-        return self._create_user(name, email, password, **extra_fields)
+        return self._create_user(email, password, **extra_fields)
 
 
 class User(AbstractUser, PermissionsMixin):
