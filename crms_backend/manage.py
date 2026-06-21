9a1eb320f663067d2d02cdfc77b79b9b98614a6e
tomcalientodev <tc298261@gmail.com>
2026-06-20 18:26:56 -0600

    manage.py env

--- crms_backend/manage.py
+++ crms_backend/manage.py
@@ -4,19 +4,49 @@ import os
 import sys
 
 
+import os
+import sys
+
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crms_backend.settings')
+
+    env = os.getenv("DJANGO_ENV", "dev") 
+
+    if env == "production":
+        settings_module = "crms_backend.settings.prod"
+    else:
+        settings_module = "crms_backend.settings.dev"
+
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
+            "Couldn't import Django..."
         ) from exc
+
     execute_from_command_line(sys.argv)
 
 
 if __name__ == '__main__':
     main()
+
+
+
+# def main():
+#     """Run administrative tasks."""
+#     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crms_backend.settings')
+#     try:
+#         from django.core.management import execute_from_command_line
+#     except ImportError as exc:
+#         raise ImportError(
+#             "Couldn't import Django. Are you sure it's installed and "
+#             "available on your PYTHONPATH environment variable? Did you "
+#             "forget to activate a virtual environment?"
+#         ) from exc
+#     execute_from_command_line(sys.argv)
+
+
+# if __name__ == '__main__':
+#     main()
